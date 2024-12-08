59957fac89504b2a156b005ff55c7eb27ef59aac
Atul Vishwakarma <atulvishw240@gmail.com>
2024-12-08 14:48:40 +0530

    Implement hash method to hash the key

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -7,4 +7,13 @@ class HashMap
     @buckets = Array.new(@capacity)
     @load_factor = 0.75
   end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = hash_code * prime_number + char.ord }
+
+    hash_code
+  end
 end
